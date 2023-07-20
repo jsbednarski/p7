7ce42d0d3304b55662b8dec11ebcc9f105ff7ab0
jsbednarski <128535436+jsbednarski@users.noreply.github.com>
2023-07-20 12:14:13 +0200

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -63,7 +63,7 @@ export default async function handler(
     },
     body: JSON.stringify({
       version:
-        "42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b",
+        "9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3",
       input: { img: imageUrl, version: "v1.4", scale: 2 },
     }),
   });
