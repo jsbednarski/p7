bb8ee92e806cc9a326b7a0f189a891292a07b4db
jsbednarski <128535436+jsbednarski@users.noreply.github.com>
2023-07-20 12:16:33 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import { Testimonials } from "../components/Testimonials";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen" style={{ backgroundColor: "#FDF2E9" }}>
       <Head>
         <title>Fotomagia</title>
       </Head>
@@ -19,13 +19,13 @@ const Home: NextPage = () => {
           href=""
           target="_blank"
           rel="noreferrer"
-          className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
+          className="border rounded-2xl py-1 px-4 text-goldenrod-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
         >
           Uwielbiany przez <span className="font-semibold">wielu</span> zadowolonych użytkowników
         </a>
-        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-black sm:text-7xl">
           Poprawianie zdjęć{" "}
-          <span className="relative whitespace-nowrap text-[#3290EE]">
+          <span className="relative whitespace-nowrap text-white">
             <SquigglyLines />
             <span className="relative">za pomocą AI</span>
           </span>{" "}
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
         </h1>
 
         {/* Comment: Main description */}
-        <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">
+        <p className="mx-auto mt-12 max-w-xl text-lg text-black leading-7">
           Masz stary, rozmazany zdjęcie? Pozwól naszej zaawansowanej sztucznej inteligencji podnieść jakość tych wspomnień i przywrócić im nowe życie.
           Bez żadnych opłat - odzyskaj już dziś swoje zdjęcia sprzed lat.
         </p>
@@ -41,7 +41,7 @@ const Home: NextPage = () => {
         <div className="flex justify-center space-x-4">
           {/* Comment: Learn more button */}
           <a
-            className="bg-white rounded-xl text-black font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-gray-100 border"
+            className="bg-goldenrod-500 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-goldenrod-600 border"
             href="bednarski.ch"
             target="_blank"
             rel="noreferrer"
@@ -51,7 +51,7 @@ const Home: NextPage = () => {
 
           {/* Comment: Restore photos button */}
           <Link
-            className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
+            className="bg-white rounded-xl text-goldenrod-500 font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-white/80"
             href="/restore"
           >
             Napraw swoje zdjęcia
@@ -63,7 +63,7 @@ const Home: NextPage = () => {
             <div className="flex sm:space-x-2 sm:flex-row flex-col">
               <div>
                 {/* Comment: Original photo */}
-                <h2 className="mb-1 font-medium text-lg">Oryginalne zdjęcie</h2>
+                <h2 className="mb-1 font-medium text-lg text-goldenrod-500">Oryginalne zdjęcie</h2>
                 <Image
                   alt="Oryginalne zdjęcie mojego brata"
                   src="/michael.jpg"
@@ -74,7 +74,7 @@ const Home: NextPage = () => {
               </div>
               <div className="sm:mt-0 mt-8">
                 {/* Comment: Fixed photo */}
-                <h2 className="mb-1 font-medium text-lg">Naprawione zdjęcie</h2>
+                <h2 className="mb-1 font-medium text-lg text-goldenrod-500">Naprawione zdjęcie</h2>
                 <Image
                   alt="Naprawione zdjęcie mojego brata"
                   width={400}
