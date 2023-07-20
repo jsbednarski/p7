d895945b26ba6b8ee207b45cd53260b18c7bb5c7
jsbednarski <128535436+jsbednarski@users.noreply.github.com>
2023-07-20 12:33:01 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,45 +11,41 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Fotomagia</title>
+        <title>Naprawiacz twarzy</title>
       </Head>
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
         <a
-          href=""
+          href="https://twitter.com/nutlope/status/1626074563481051136"
           target="_blank"
           rel="noreferrer"
           className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
         >
-          Uwielbiany przez <span className="font-semibold">wielu</span> zadowolonych użytkowników
+          Uwielbiany przez <span className="font-semibold">wiele</span> zadowolonych użytkowników
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
-          Poprawianie zdjęć{" "}
+          Naprawianie twarzy{" "}
           <span className="relative whitespace-nowrap text-[#3290EE]">
             <SquigglyLines />
-            <span className="relative">za pomocą AI</span>
+            <span className="relative">za pomoca AI</span>
           </span>{" "}
-          nawet dla pięknych osób.
+          nawet dla ładnych osób.
         </h1>
 
-        {/* Comment: Main description */}
         <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">
-          Masz stary, rozmazany zdjęcie? Pozwól naszej zaawansowanej sztucznej inteligencji podnieść jakość tych wspomnień i przywrócić im nowe życie.
-          Bez żadnych opłat - odzyskaj już dziś swoje zdjęcia sprzed lat.
+          Czy masz starą, rozmytą fotografię twarzy? Pozwól naszej zaawansowanej sztucznej inteligencji przywrócić te wspomnienia i ożywić je na nowo. 
+          Bez żadnych kosztów - przywróć swoje zdjęcia sprzed lat już dzisiaj.
         </p>
-        
         <div className="flex justify-center space-x-4">
-          {/* Comment: Learn more button */}
           <a
             className="bg-white rounded-xl text-black font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-gray-100 border"
             href="bednarski.ch"
             target="_blank"
             rel="noreferrer"
           >
-            Dowiedz się więcej
+            dowiedz sie wiecej
           </a>
 
-          {/* Comment: Restore photos button */}
           <Link
             className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
             href="/restore"
@@ -56,15 +53,13 @@ const Home: NextPage = () => {
             Napraw swoje zdjęcia
           </Link>
         </div>
-
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-2 sm:flex-row flex-col">
               <div>
-                {/* Comment: Original photo */}
-                <h2 className="mb-1 font-medium text-lg">Oryginalne zdjęcie</h2>
+                <h2 className="mb-1 font-medium text-lg">Zdjęcie oryginalne</h2>
                 <Image
-                  alt="Oryginalne zdjęcie mojego brata"
+                  alt="Original photo of my bro"
                   src="/michael.jpg"
                   className="w-96 h-96 rounded-2xl"
                   width={400}
@@ -72,10 +67,9 @@ const Home: NextPage = () => {
                 />
               </div>
               <div className="sm:mt-0 mt-8">
-                {/* Comment: Fixed photo */}
                 <h2 className="mb-1 font-medium text-lg">Naprawione zdjęcie</h2>
                 <Image
-                  alt="Naprawione zdjęcie mojego brata"
+                  alt="Restored photo of my bro"
                   width={400}
                   height={400}
                   src="/michael-new.jpg"
