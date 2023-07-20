bc11dc004c31a50f90c35c8dfae852c99ffb4c08
jsbednarski <128535436+jsbednarski@users.noreply.github.com>
2023-07-20 13:05:27 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
         {/* Link do udostępnionego tweetu */}
         <a
-          href="https://twitter.com/nutlope/status/1626074563481051136"
+          href=""
           target="_blank"
           rel="noreferrer"
           className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
