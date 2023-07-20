4790e13c028cc464760198eddcc94ae5ebadc4bf
jsbednarski <128535436+jsbednarski@users.noreply.github.com>
2023-07-20 12:47:42 +0200

    Update CompareSlider.tsx

--- components/CompareSlider.tsx
+++ components/CompareSlider.tsx
@@ -4,18 +4,18 @@ import {
 } from "react-compare-slider";
 
 export const CompareSlider = ({
-  original,
-  restored,
+  original, // Źródło zdjęcia oryginalnego
+  restored, // Źródło zdjęcia po odrestaurowaniu
 }: {
   original: string;
   restored: string;
 }) => {
   return (
     <ReactCompareSlider
-      itemOne={<ReactCompareSliderImage src={original} alt="original photo" />}
-      itemTwo={<ReactCompareSliderImage src={restored} alt="restored photo" />}
-      portrait
-      className="flex w-[475px] mt-5"
+      itemOne={<ReactCompareSliderImage src={original} alt="oryginalne zdjęcie" />} // Wyświetl oryginalne zdjęcie
+      itemTwo={<ReactCompareSliderImage src={restored} alt="odrestaurowane zdjęcie" />} // Wyświetl odrestaurowane zdjęcie
+      portrait // Ustaw orientację suwaka na pionową
+      className="flex w-[475px] mt-5" // Zastosuj podane klasy CSS do kontenera suwaka porównawczego
     />
   );
 };
